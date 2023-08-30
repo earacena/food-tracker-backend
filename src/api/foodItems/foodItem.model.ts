9d301a2cc1420c7a2c64a8c1daaf52da108d7b9f
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-08-30 16:57:47 -0400

    feat(foodItem): implement foodItem model

---
+++ src/api/foodItems/foodItem.model.ts
@@ -0,0 +1,22 @@
+import type {
+  ColumnType,
+  Generated,
+  Insertable,
+  Selectable,
+  Updateable
+} from 'kysely'
+
+export interface FoodItemTable {
+  id: Generated<number>
+  foodName: string
+  userId: number
+  caloriesPerServing: number
+  servingSizeInGrams: number | null
+  servingSizeInUnits: number | null
+  searchVisibility: 'public' | 'private'
+  createdAt: ColumnType<Date, string | undefined, never>
+};
+
+export type FoodItem = Selectable<FoodItemTable>
+export type NewFoodItem = Insertable<FoodItemTable>
+export type FoodItemUpdate = Updateable<FoodItemTable>
