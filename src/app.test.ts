d1bbb2de95f79423c8b216e6376c5f505bd6d3bf
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-08-30 16:53:32 -0400

    style: auto format

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,13 +1,13 @@
-import supertest from 'supertest';
-import app from './app';
+import supertest from 'supertest'
+import app from './app'
 
-const api = supertest(app.expressApp);
+const api = supertest(app.expressApp)
 
 test('simple app test', async () => {
   const response = await api
     .get('/')
-    .expect(200);
+    .expect(200)
 
-  const responseData = JSON.parse(response.text);
-  expect(responseData).toStrictEqual({ data: 'Hello World!' });
-});
+  const responseData = JSON.parse(response.text)
+  expect(responseData).toStrictEqual({ data: 'Hello World!' })
+})
