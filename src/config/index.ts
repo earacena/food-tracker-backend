5aead04546f591062aadc745d99c7762ea7573c7
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-08-30 16:57:35 -0400

    feat(config): implement env variable processing

---
+++ src/config/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+export const NODE_ENV = z.string().parse(process.env['NODE_ENV'])
+
+const zDatabaseCredentials = z.object({
+  user: z.string(),
+  pass: z.string(),
+  host: z.string(),
+  port: z.coerce.number(),
+  name: z.string()
+})
+
+export const databaseCredentials = zDatabaseCredentials.parse({
+  user: process.env['DATABASE_USER'],
+  pass: process.env['DATABASE_PASS'],
+  host: process.env['DATABASE_HOST'],
+  port: process.env['DATABASE_PORT'],
+  name: process.env['DATABASE_NAME']
+})
