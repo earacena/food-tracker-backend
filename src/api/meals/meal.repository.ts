242f10c03b3e7ccea7523ad78f8619f01bb56daf
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-08-31 15:44:06 -0400

    feat(meal): implement meal repository functions

---
+++ src/api/meals/meal.repository.ts
@@ -0,0 +1,46 @@
+import { db } from '../../utils/db'
+import { type NewMeal, type Meal, type MealUpdate } from './meal.model'
+
+export async function findMealById (id: number): Promise<Meal> {
+  return await db.selectFrom('meal')
+    .where('id', '=', id)
+    .selectAll()
+    .executeTakeFirstOrThrow()
+}
+
+export async function findMealsByUserId (userId: number): Promise<Meal[]> {
+  return await db.selectFrom('meal')
+    .where('userId', '=', userId)
+    .selectAll()
+    .execute()
+}
+
+export async function createMeal (meal: NewMeal): Promise<Meal> {
+  return await db.insertInto('meal')
+    .values(meal)
+    .returningAll()
+    .executeTakeFirstOrThrow()
+}
+
+export async function updateMeal (id: number, updateWith: MealUpdate): Promise<Meal> {
+  return await db.updateTable('meal')
+    .set(updateWith)
+    .where('id', '=', id)
+    .returningAll()
+    .executeTakeFirstOrThrow()
+}
+
+export async function deleteMeal (id: number): Promise<void> {
+  await db.deleteFrom('meal')
+    .where('id', '=', id)
+    .returningAll()
+    .executeTakeFirst()
+}
+
+export default {
+  findMealById,
+  findMealsByUserId,
+  createMeal,
+  updateMeal,
+  deleteMeal
+}
