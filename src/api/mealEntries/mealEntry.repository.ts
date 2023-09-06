9d4d827919611f76ce524ea21632f7ccab510d7b
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-06 00:02:55 -0400

    feat(mealEntry): add deleting and fetching by id

--- src/api/mealEntries/mealEntry.repository.ts
+++ src/api/mealEntries/mealEntry.repository.ts
@@ -1,6 +1,13 @@
 import { db } from '../../utils/db'
 import { type MealEntry } from './mealEntry.model'
 
+export async function findMealEntryById (id: number): Promise<MealEntry> {
+  return await db.selectFrom('mealEntry')
+    .where('id', '=', id)
+    .selectAll()
+    .executeTakeFirstOrThrow()
+}
+
 export async function findAllMealEntriesByMealId (mealId: number): Promise<MealEntry[]> {
   return await db.selectFrom('mealEntry')
     .where('mealId', '=', mealId)
@@ -8,6 +15,15 @@ export async function findAllMealEntriesByMealId (mealId: number): Promise<MealE
     .execute()
 }
 
+export async function deleteMealEntry (id: number): Promise<void> {
+  await db.deleteFrom('mealEntry')
+    .where('id', '=', id)
+    .returningAll()
+    .executeTakeFirst()
+}
+
 export default {
-  findAllMealEntriesByMealId
+  findAllMealEntriesByMealId,
+  findMealEntryById,
+  deleteMealEntry
 }
