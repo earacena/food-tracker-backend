b922154e67f19c1b3606d1d5f8d1d8d8a55acd6b
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-06 22:41:55 -0400

    feat(mealEntry): implement creating mealEntry

--- src/api/mealEntries/mealEntry.repository.ts
+++ src/api/mealEntries/mealEntry.repository.ts
@@ -1,5 +1,5 @@
 import { db } from '../../utils/db'
-import { type MealEntry } from './mealEntry.model'
+import { type NewMealEntry, type MealEntry } from './mealEntry.model'
 
 export async function findMealEntryById (id: number): Promise<MealEntry> {
   return await db.selectFrom('mealEntry')
@@ -15,6 +15,13 @@ export async function findAllMealEntriesByMealId (mealId: number): Promise<MealE
     .execute()
 }
 
+export async function createMealEntry (mealEntry: NewMealEntry): Promise<MealEntry> {
+  return await db.insertInto('mealEntry')
+    .values(mealEntry)
+    .returningAll()
+    .executeTakeFirstOrThrow()
+}
+
 export async function deleteMealEntry (id: number): Promise<void> {
   await db.deleteFrom('mealEntry')
     .where('id', '=', id)
@@ -25,5 +32,6 @@ export async function deleteMealEntry (id: number): Promise<void> {
 export default {
   findAllMealEntriesByMealId,
   findMealEntryById,
+  createMealEntry,
   deleteMealEntry
 }
