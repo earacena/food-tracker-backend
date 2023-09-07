dfc7e59ebb00403a12bd958818c7f38865f61b6c
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-06 22:40:40 -0400

    fix(mealEntry): make foodItemId and mealId nullable

--- src/api/mealEntries/mealEntry.types.ts
+++ src/api/mealEntries/mealEntry.types.ts
@@ -3,12 +3,18 @@ import { z } from 'zod'
 export const zMealEntry = z.object({
   id: z.number(),
   userId: z.number(),
-  foodItemId: z.number(),
-  mealId: z.number()
+  foodItemId: z.number().nullable(),
+  mealId: z.number().nullable()
 })
 
 export const zMealEntries = z.array(zMealEntry)
 
 export const zMealIdParams = z.object({
   mealId: z.coerce.number()
+})
+
+export const zMealEntryDetails = z.object({
+  userId: z.number(),
+  foodItem: z.number().nullable(),
+  mealId: z.number().nullable()
 })
