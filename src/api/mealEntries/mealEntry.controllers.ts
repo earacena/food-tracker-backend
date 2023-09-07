dcbfa6b74cd464ebbeb55de625efdb7a62dcea93
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-06 22:43:33 -0400

    feat(mealEntry): add controller for creating mealEntry

--- src/api/mealEntries/mealEntry.controllers.ts
+++ src/api/mealEntries/mealEntry.controllers.ts
@@ -3,13 +3,12 @@ import type {
   Request,
   Response
 } from 'express'
-import { zMealEntries, zMealIdParams } from './mealEntry.types'
+import { zMealEntries, zMealEntry, zMealEntryDetails, zMealIdParams } from './mealEntry.types'
 import MealEntryRepository from './mealEntry.repository'
 
 export async function getMealEntriesByMealIdController (req: Request, res: Response, next: NextFunction): Promise<void> {
   try {
     const { mealId } = zMealIdParams.parse(req.params)
-
     const mealEntries = zMealEntries.parse(await MealEntryRepository.findAllMealEntriesByMealId(mealId))
 
     res.status(200)
@@ -17,6 +16,23 @@ export async function getMealEntriesByMealIdController (req: Request, res: Respo
         success: true,
         data: {
           mealEntries
+        }
+      })
+  } catch (err: unknown) {
+    next(err)
+  }
+}
+
+export async function createMealEntryController (req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const mealEntryDetails = zMealEntryDetails.parse(req.body)
+    const newMealEntry = zMealEntry.parse(await MealEntryRepository.createMealEntry(mealEntryDetails))
+
+    res.status(201)
+      .json({
+        success: true,
+        data: {
+          newMealEntry
         }
       })
   } catch (err: unknown) {
