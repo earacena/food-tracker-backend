e18b6b65b5e62007450dc5c62cfd8e1e0ed633ec
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-10 02:06:40 -0400

    refactor(error): re-export new errors

--- src/utils/errors/index.ts
+++ src/utils/errors/index.ts
@@ -1,3 +1,5 @@
 export { default as FoodItemNotFoundError } from './FoodItemNotFoundError'
 export { default as MealEntryNotFoundError } from './MealEntryNotFoundError'
 export { default as MealNotFoundError } from './MealNotFoundError'
+export { default as AuthenticationError } from './AuthenticationError'
+export { default as ProfileNotFoundError } from './ProfileNotFoundError'
