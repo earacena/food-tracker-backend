2f3aaf84ecebc2e35d35f41829d668d59586718c
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-10 02:04:34 -0400

    feat(profile): implement profile repository

---
+++ src/api/profiles/profile.repository.ts
@@ -0,0 +1,39 @@
+import { db } from '../../utils/db'
+import { ProfileNotFoundError } from '../../utils/errors'
+import { type ProfileUpdate, type NewProfile, type Profile } from './profile.model'
+
+export async function findProfileById (userId: string): Promise<Profile> {
+  return await db.selectFrom('profile')
+    .where('userId', '=', userId)
+    .selectAll()
+    .executeTakeFirstOrThrow(() => new ProfileNotFoundError('profile not found'))
+}
+
+export async function createProfile (profile: NewProfile): Promise<Profile> {
+  return await db.insertInto('profile')
+    .values(profile)
+    .returningAll()
+    .executeTakeFirstOrThrow()
+}
+
+export async function updateProfile (userId: string, updateWith: ProfileUpdate): Promise<Profile> {
+  return await db.updateTable('profile')
+    .set(updateWith)
+    .where('userId', '=', userId)
+    .returningAll()
+    .executeTakeFirstOrThrow()
+}
+
+export async function deleteProfile (userId: string): Promise<void> {
+  await db.deleteFrom('profile')
+    .where('userId', '=', userId)
+    .returningAll()
+    .executeTakeFirst()
+}
+
+export default {
+  findProfileById,
+  createProfile,
+  updateProfile,
+  deleteProfile
+}
