6c0654aacaea468d2b1396de0e87e3b75a58b5e8
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-11 16:08:28 -0400

    feat(db): create schema for postgres database

---
+++ src/config/db/schema.sql
@@ -0,0 +1,40 @@
+CREATE TYPE visibility AS ENUM ('private', 'public');
+
+CREATE TABLE foodItem (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  foodName TEXT NOT NULL,
+  userId uuid NOT NULL,
+  caloriesPerServing INTEGER,
+  servingSizeInGrams INTEGER,
+  servingSizeInUnits INTEGER,
+  searchVisibility visibility,
+  createdAt timestampz DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE meal (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  userId uuid NOT NULL,
+  name TEXT NOT NULL,
+  createdAt timestampz DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE mealEntry (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  userId uuid NOT NULL,
+  mealId INTEGER,
+  foodItemId INTEGER
+);
+
+CREATE TABLE profile (
+  userId uuid PRIMARY KEY,
+  dailyCalorieGoal INTEGER,
+  createdAt timestampz DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE activity (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  userId uuid,
+  mealId INTEGER,
+  foodItemId: INTEGER,
+  createdAt timestampz DEFAULT CURRENT_TIMESTAMP
+);
