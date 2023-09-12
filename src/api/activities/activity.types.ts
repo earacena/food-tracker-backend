b20e1c8e720b84a0f2655376a9dbb651354e3776
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-11 22:04:34 -0400

    feat(activity): add type parser

--- src/api/activities/activity.types.ts
+++ src/api/activities/activity.types.ts
@@ -5,7 +5,15 @@ export const zActivity = z.object({
   userId: z.string().uuid(),
   foodItemId: z.number().nullable(),
   mealId: z.number().nullable(),
+  quantity: z.number(),
   createdAt: z.coerce.date()
 })
 
 export const zActivities = z.array(zActivity)
+
+export const zActivityDetails = z.object({
+  userId: z.string().uuid(),
+  foodItemId: z.number().nullable(),
+  mealId: z.number().nullable(),
+  quantity: z.number()
+})
