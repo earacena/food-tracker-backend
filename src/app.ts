c5315f7be3f5225247dcd5f42e0c08e232f42faf
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-13 01:28:42 -0400

    feat(app): integrate profileRouter into app

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import foodItemRouter from './api/foodItems/foodItem.routes'
 import errorHandler from './middleware/errorHandler.middleware'
 import mealRouter from './api/meals/meal.routes'
 import mealEntryRouter from './api/mealEntries/mealEntry.routes'
+import authenticate from './middleware/authenticate'
+import profileRouter from './api/profiles/profile.routes'
+import activityRouter from './api/activities/activity.routes'
 
 const expressApp = express()
 
@@ -11,9 +14,11 @@ const expressApp = express()
 expressApp.use(express.json())
 
 // Routes
-expressApp.use('/api/foodItems', foodItemRouter)
-expressApp.use('/api/meals', mealRouter)
-expressApp.use('/api/mealEntries', mealEntryRouter)
+expressApp.use('/api/activities', authenticate, activityRouter)
+expressApp.use('/api/foodItems', authenticate, foodItemRouter)
+expressApp.use('/api/meals', authenticate, mealRouter)
+expressApp.use('/api/mealEntries', authenticate, mealEntryRouter)
+expressApp.use('/api/profiles', authenticate, profileRouter)
 
 // Post-route middleware
 expressApp.use(errorHandler)
