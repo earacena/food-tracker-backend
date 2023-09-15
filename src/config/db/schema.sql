a1c6fc29d96eeacd5e8d7f81b9e669de24f7c4e7
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-14 21:23:25 -0400

    refactor(db): make column names case sensitive

--- src/config/db/schema.sql
+++ src/config/db/schema.sql
@@ -1,41 +1,41 @@
 CREATE TYPE visibility AS ENUM ('private', 'public');
 
 CREATE TABLE foodItem (
-  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  foodName TEXT NOT NULL,
-  userId uuid NOT NULL,
-  caloriesPerServing INTEGER,
-  servingSizeInGrams INTEGER,
-  servingSizeInUnits INTEGER,
-  searchVisibility visibility,
-  createdAt timestampz DEFAULT CURRENT_TIMESTAMP
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "foodName" TEXT NOT NULL,
+  "userId" uuid NOT NULL,
+  "caloriesPerServing" INTEGER,
+  "servingSizeInGrams" INTEGER,
+  "servingSizeInUnits" INTEGER,
+  "searchVisibility" visibility,
+  "createdAt" timestamptz DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE meal (
-  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  userId uuid NOT NULL,
-  name TEXT NOT NULL,
-  createdAt timestampz DEFAULT CURRENT_TIMESTAMP
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "userId" uuid NOT NULL,
+  "name" TEXT NOT NULL,
+  "createdAt" timestamptz DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE mealEntry (
-  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  userId uuid NOT NULL,
-  mealId INTEGER,
-  foodItemId INTEGER,
-  quantity INTEGER
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "userId" uuid NOT NULL,
+  "mealId" INTEGER,
+  "foodItemId" INTEGER,
+  "quantity" INTEGER
 );
 
 CREATE TABLE profile (
-  userId uuid PRIMARY KEY,
-  dailyCalorieGoal INTEGER,
-  createdAt timestampz DEFAULT CURRENT_TIMESTAMP
+  "userId" uuid PRIMARY KEY,
+  "dailyCalorieGoal" INTEGER,
+  "createdAt" timestamptz DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE activity (
-  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  userId uuid,
-  mealId INTEGER,
-  foodItemId: INTEGER,
-  createdAt timestampz DEFAULT CURRENT_TIMESTAMP
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "userId" uuid,
+  "mealId" INTEGER,
+  "foodItemId" INTEGER,
+  "createdAt" timestamptz DEFAULT CURRENT_TIMESTAMP
 );
