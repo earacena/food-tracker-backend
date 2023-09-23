ab74155211a5565982460436e811a37efbf030f7
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-23 04:42:02 -0400

    refactor(error): make auth error extend ApiError

--- src/utils/errors/AuthenticationError.ts
+++ src/utils/errors/AuthenticationError.ts
@@ -1,3 +1,15 @@
-class AuthenticationError extends Error {}
+import { type Response } from 'express'
+import ApiError from './ApiError'
+
+class AuthenticationError extends ApiError {
+  override respond (res: Response): Response {
+    return res
+      .status(401)
+      .json({
+        success: false,
+        errorMessage: this.message
+      })
+  }
+}
 
 export default AuthenticationError
